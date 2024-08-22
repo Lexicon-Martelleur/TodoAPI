8d977739210de0cc34c36b8de897b90750c24bcb
Joel Martelleur <joel.martelleur@gmail.com>
2024-08-22 09:34:31 +0200

    Update structure in program file

--- TodoAPI/Program.cs
+++ TodoAPI/Program.cs
@@ -21,17 +21,12 @@ public class Program
 
     private static WebApplication CreateWebApplication (string[] args)
     {
-        Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Debug()
-            .WriteTo.Console()
-            .WriteTo.File("logs/todoinfo.txt", rollingInterval: RollingInterval.Day)
-            .CreateLogger();
 
         var builder = WebApplication.CreateBuilder(args);
 
-        builder.Host.UseSerilog();
+        AddCustomLogging(builder);
 
-        builder.Services.AddProblemDetails();
+        AddCustomGlobalErrorHandling(builder);
 
         builder.Services.AddControllers(options =>
         {
@@ -59,8 +54,7 @@ public class Program
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-
-        if (!app.Environment.IsDevelopment())
+        else
         {
             app.UseHttpsRedirection();
             app.UseExceptionHandler();
@@ -71,6 +65,39 @@ public class Program
         app.MapControllers();
 
         app.Run();
+    }
+
+    private static void AddCustomLogging(WebApplicationBuilder builder)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("logs/todoinfo.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+        builder.Host.UseSerilog();
+    }
+
+    private static void AddCustomGlobalErrorHandling(WebApplicationBuilder builder)
+    {
+        builder.Services.AddProblemDetails(options =>
+        {
+            options.CustomizeProblemDetails = context =>
+            {
+                if (context.Exception is NotImplementedException)
+                {
+                    context.ProblemDetails.Status = StatusCodes.Status503ServiceUnavailable;
+                    context.ProblemDetails.Title = "Resource not implemented";
+                    context.ProblemDetails.Detail = context.Exception.Message;
+                }
+                else
+                {
+                    context.ProblemDetails.Status = StatusCodes.Status500InternalServerError;
+                    context.ProblemDetails.Title = "An unexpected error occurred";
+                    context.ProblemDetails.Detail = "Please try again later.";
+                }
+            };
+        });
     }
 
     private static void AddAppServices (WebApplicationBuilder builder)
